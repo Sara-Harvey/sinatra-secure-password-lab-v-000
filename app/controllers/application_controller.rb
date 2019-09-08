73899fae30e369e473d1cdb306d81b7995bf06b4
Sara Harvey <saraharveynyc@gmail.com>
2019-09-08 23:23:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
       redirect to '/failure'
     end
   
-    user = User.new(:username => params[:username], :password => params[:password] )
+    user = User.new(:username => params[:username], :password => params[:password])
   
 
   get '/account' do
